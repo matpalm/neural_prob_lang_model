79f8ed9662c8470f08de8c0fab509b71773a0d5e
mat kelcey <matthew.kelcey@gmail.com>
2015-05-19 21:40:30 -0700

    change to using a list to make generating space seperated sequences easier

--- rnn_lm/generate.py
+++ rnn_lm/generate.py
@@ -8,22 +8,23 @@ import random, sys
 def coin_flip():
     return 0 if random.random() < 0.5 else 1
 
-def reber_string():
+def reber_sequence():
     transistions = { 0: [(1, "T"), (3, "P")],
                      1: [(1, "S"), (2, "X")],
                      2: [(3, "X"), (5, "S")],
                      3: [(3, "T"), (4, "V")],
                      4: [(2, "P"), (5, "V")] }
-    state, s = 0, "B"
+    state, s = 0, ["B"]
     while state != 5:
         transistion = transistions[state][coin_flip()]
         state = transistion[0]
-        s += transistion[1]
-    return s + "E"
+        s.append(transistion[1])
+    s.append("E")
+    return s
 
-def embedded_reber_string():
+def embedded_reber_sequence():
     path = "T" if coin_flip() else "P"
-    return "B" + path + reber_string() + path + "E"
+    return ["B"] + [path] + reber_sequence() + [path] + ["E"]
 
 
 num_to_generate = None
@@ -33,4 +34,4 @@ except:
     raise Exception("usage: " + sys.argv[0] + " num_strings_to_generate")
 
 for _ in xrange(0, num_to_generate):
-    print embedded_reber_string()
+    print "".join(embedded_reber_sequence())
